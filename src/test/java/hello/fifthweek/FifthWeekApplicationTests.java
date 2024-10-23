28a006fc94cc336810c394a8eb21a979623f8f78
kiset200 <kiset200@jcobinfo.com>
2024-10-23 16:37:26 +0900

    Initial commit

---
+++ src/test/java/hello/fifthweek/FifthWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.fifthweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifthWeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
